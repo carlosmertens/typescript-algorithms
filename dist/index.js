dfabdfba6cb280ca2f32dfde93c48cf15166431f
Carlos Mertens <mertenscarlos3000@gmail.com>
2023-10-15 17:22:07 +0200

    Create debuggin file

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,6 @@
 "use strict";
 let age = 44;
+if (age < 50)
+    age += 10;
 console.log(`My age is ${age}`);
+//# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
 let age: number = 44;
 
+if (age < 50) {
+  age += 10;
+}
+
 console.log(`My age is ${age}`);
