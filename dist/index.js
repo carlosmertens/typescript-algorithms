93b3165c75abcb82a11ca718e119022ca3633081
Carlos Mertens <mertenscarlos3000@gmail.com>
2023-10-19 15:46:38 +0200

    Learn basic annotations

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,41 @@
 "use strict";
 let age = 44;
-if (age < 50)
+if (age < 50) {
     age += 10;
+}
 console.log(`My age is ${age}`);
+let sales = 950000000;
+let course = 'TypeScript';
+let published = true;
+let income = 2000;
+let firstName = 'Carlos';
+let numbers = [1, 2, 3];
+const letters = ['a', 'b', 'c'];
+let ages = [18, 25, 23];
+const vowels = ['a', 'e', 'i', 'o', 'u'];
+const user = [1, 'Mertens'];
+const skill = [true, 'TypeScript'];
+let mysize = 2;
+console.log(mysize);
+function calculateTax(income, age = 30) {
+    const factor = 1.2;
+    if (age > 30) {
+        return income * factor;
+    }
+    return income;
+}
+console.log(calculateTax(3000, 35));
+console.log(calculateTax(3000));
+function render(document) {
+    console.log(document);
+}
+const employee = {
+    id: 1,
+    name: 'Edgar',
+    retire: (date) => {
+        console.log(date);
+    },
+};
+employee.name = 'Mertens';
+console.log(employee);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,68 @@ if (age < 50) {
 }
 
 console.log(`My age is ${age}`);
+
+// Annotate primitive values
+let sales: number = 950_000_000;
+let course: string = 'TypeScript';
+let published: boolean = true;
+
+let income = 2_000;
+let firstName = 'Carlos';
+
+// Annotate arrays
+let numbers: number[] = [1, 2, 3];
+const letters: string[] = ['a', 'b', 'c'];
+
+let ages = [18, 25, 23];
+const vowels = ['a', 'e', 'i', 'o', 'u'];
+
+// Annotate tuples
+const user: [number, string] = [1, 'Mertens'];
+const skill: [boolean, string] = [true, 'TypeScript'];
+
+// Annotate enums
+const enum Size {
+  Small = 1,
+  Medium,
+  Large,
+}
+
+let mysize: Size = Size.Medium;
+console.log(mysize);
+
+// Annotate functions
+function calculateTax(income: number, age = 30): number {
+  const factor = 1.2;
+  if (age > 30) {
+    return income * factor;
+  }
+
+  return income;
+}
+
+console.log(calculateTax(3000, 35));
+console.log(calculateTax(3000));
+
+// Annotate functions with any type
+function render(document: any) {
+  console.log(document);
+}
+
+// Annotate objects
+
+const employee: {
+  readonly id: number;
+  name: string;
+  retire: (date: Date) => void;
+} = {
+  id: 1,
+  name: 'Edgar',
+  retire: (date: Date) => {
+    console.log(date);
+  },
+};
+
+employee.name = 'Mertens';
+
+console.log(employee);
