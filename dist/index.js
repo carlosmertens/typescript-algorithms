a7bc90f8e785c9f9d02111c05cf5f48e531856b4
Carlos Mertens <mertenscarlos3000@gmail.com>
2023-10-24 15:37:45 +0200

    Create examples of all fundamental annotations

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,6 @@
 "use strict";
+var _a;
+console.log('////////// Types Annotations //////////');
 let age = 44;
 if (age < 50) {
     age += 10;
@@ -38,4 +40,53 @@ const employee = {
 };
 employee.name = 'Mertens';
 console.log(employee);
+const employee1 = {
+    id: 2,
+    name: 'Peter',
+    retire: (date) => {
+        console.log(date);
+    },
+};
+function kgToLbs(weight) {
+    if (typeof weight === 'number')
+        return weight * 2.2;
+    else
+        return parseInt(weight) * 2.2;
+}
+console.log(kgToLbs(10));
+console.log(kgToLbs('10kg'));
+const textBox = {
+    drag: () => { },
+    rezise: () => { },
+};
+let quantityA = 100;
+let quantityb = 50;
+function upperCaseWord(str) {
+    if (str)
+        return str.toUpperCase();
+    else
+        return 'Life is unexpected!';
+}
+upperCaseWord('carlos');
+upperCaseWord(null);
+function getCustomer(id) {
+    return id === 0 ? null : { birthday: new Date() };
+}
+const customer = getCustomer(1);
+console.log((_a = customer === null || customer === void 0 ? void 0 : customer.birthday) === null || _a === void 0 ? void 0 : _a.getFullYear());
+const arr = [1, 2, 3];
+console.log(arr === null || arr === void 0 ? void 0 : arr[4]);
+const log = null;
+log === null || log === void 0 ? void 0 : log('a');
+let speed = null;
+let ride = {
+    speed: speed !== null && speed !== void 0 ? speed : 30,
+};
+function renderDocument(document) {
+    console.log(typeof document);
+}
+function processEvent() {
+    while (true) {
+    }
+}
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+console.log('////////// Types Annotations //////////');
+
 let age: number = 44;
 
 if (age < 50) {
@@ -6,7 +8,7 @@ if (age < 50) {
 
 console.log(`My age is ${age}`);
 
-// Annotate primitive values
+// ANNOTATE PRIMITIVE VALUES
 let sales: number = 950_000_000;
 let course: string = 'TypeScript';
 let published: boolean = true;
@@ -14,18 +16,18 @@ let published: boolean = true;
 let income = 2_000;
 let firstName = 'Carlos';
 
-// Annotate arrays
+// ANNOTATE ARRAYS
 let numbers: number[] = [1, 2, 3];
 const letters: string[] = ['a', 'b', 'c'];
 
 let ages = [18, 25, 23];
 const vowels = ['a', 'e', 'i', 'o', 'u'];
 
-// Annotate tuples
+// ANNOTATE TUPLES
 const user: [number, string] = [1, 'Mertens'];
 const skill: [boolean, string] = [true, 'TypeScript'];
 
-// Annotate enums
+// ANNOTATE ENUMS
 const enum Size {
   Small = 1,
   Medium,
@@ -35,7 +37,7 @@ const enum Size {
 let mysize: Size = Size.Medium;
 console.log(mysize);
 
-// Annotate functions
+// ANNOTATE FUNCTIONS
 function calculateTax(income: number, age = 30): number {
   const factor = 1.2;
   if (age > 30) {
@@ -48,13 +50,12 @@ function calculateTax(income: number, age = 30): number {
 console.log(calculateTax(3000, 35));
 console.log(calculateTax(3000));
 
-// Annotate functions with any type
+// FUNCTION WITH ANY TYPE
 function render(document: any) {
   console.log(document);
 }
 
-// Annotate objects
-
+// ANNOTATE OBJECTS
 const employee: {
   readonly id: number;
   name: string;
@@ -70,3 +71,111 @@ const employee: {
 employee.name = 'Mertens';
 
 console.log(employee);
+
+// TYPE ALIAS
+type Employee = {
+  readonly id: number;
+  name: string;
+  retire: (date: Date) => void;
+};
+
+const employee1: Employee = {
+  id: 2,
+  name: 'Peter',
+  retire: (date: Date) => {
+    console.log(date);
+  },
+};
+
+// UNION TYPES
+function kgToLbs(weight: number | string): number {
+  if (typeof weight === 'number') return weight * 2.2;
+  else return parseInt(weight) * 2.2;
+}
+
+console.log(kgToLbs(10));
+console.log(kgToLbs('10kg'));
+
+// TYPE INTERSECTION
+type Draggable = {
+  drag: () => void;
+};
+
+type Resizable = {
+  rezise: () => void;
+};
+
+type UIWidget = Draggable & Resizable;
+
+const textBox: UIWidget = {
+  drag: () => {},
+  rezise: () => {},
+};
+
+// LITERAL TYPES
+type Quantity = 50 | 100;
+
+let quantityA: Quantity = 100;
+let quantityb: Quantity = 50;
+
+type Metric = 'cm' | 'inch';
+
+// NULLABLE TYPES
+function upperCaseWord(str: string | null | undefined): string {
+  if (str) return str.toUpperCase();
+  else return 'Life is unexpected!';
+}
+
+upperCaseWord('carlos');
+upperCaseWord(null);
+
+// OPTIONAL CHAINING
+// Optional property operator (?.)
+type Customer = {
+  birthday?: Date;
+};
+
+function getCustomer(id: number): Customer | null | undefined {
+  return id === 0 ? null : { birthday: new Date() };
+}
+
+const customer = getCustomer(1);
+// optional property operator
+console.log(customer?.birthday?.getFullYear());
+
+const arr = [1, 2, 3];
+// optional element access operator
+console.log(arr?.[4]);
+
+const log: any = null;
+// optional call operator
+log?.('a');
+
+// NULLISH COAELSCING OPERATOR (null or undefined)
+let speed: number | null = null;
+let ride = {
+  speed: speed ?? 30, // checck for null or undefined
+};
+
+// TYPE ASSERTIONS (as)
+// let phone = document.getElementById('phone') as HTMLInputElement;
+// console.log(phone.value);
+
+// or
+// let street = <HTMLInputElement>document.getElementById('street');
+// console.log(street.value);
+
+// UNKNOWN TYPE (prefer than any type)
+
+function renderDocument(document: unknown) {
+  console.log(typeof document);
+}
+
+// NEVER TYPE
+// To annotate a function that will never return and create infinity loop
+
+function processEvent(): never {
+  while (true) {
+    // Do something
+  }
+}
